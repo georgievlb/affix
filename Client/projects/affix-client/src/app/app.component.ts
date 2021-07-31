874b7f32afc58cda70cc0125a1f4212e38493994
Lachezar Georgiev <georgiev.lb@gmail.com>
2021-07-31 17:17:39 +0300

    Add some styling.

--- Client/projects/affix-client/src/app/app.component.ts
+++ Client/projects/affix-client/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: [
-    'header { background-color: whitesmoke; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .2) }'
+    'header { background-color: white; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .2) }'
   ]
 })
 export class AppComponent {
