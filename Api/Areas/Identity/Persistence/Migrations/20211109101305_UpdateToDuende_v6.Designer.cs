861a56312750bfe29d414bc0bd91bbc5d292d998
Lachezar Georgiev <georgiev.lb@gmail.com>
2021-11-11 23:40:49 +0200

    #23 Update to Dotnet 6 and Duende Identity Server. (#52)

--- Api/Areas/Identity/Persistence/Migrations/20211109101305_UpdateToDuende_v6.Designer.cs
+++ Api/Areas/Identity/Persistence/Migrations/20211109101305_UpdateToDuende_v6.Designer.cs
@@ -4,6 +4,7 @@ using Auth.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Affix.Areas.Identity.Persistence.Migrations
 {
     [DbContext(typeof(AffixIdentityContext))]
-    partial class AffixIdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20211109101305_UpdateToDuende_v6")]
+    partial class UpdateToDuende_v6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
