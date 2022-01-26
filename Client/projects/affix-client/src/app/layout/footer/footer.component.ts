b0cb5f7067bdae328dfe871f9d945493ffd0ab10
Lachezar Georgiev <georgiev.lb@gmail.com>
2022-01-26 13:48:43 +0200

    #71 Add the application footer (#72)

--- Client/projects/affix-client/src/app/layout/footer/footer.component.ts
+++ Client/projects/affix-client/src/app/layout/footer/footer.component.ts
@@ -1,28 +1,8 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-
-  public credits: string[] = [
-      'Powered by Angular Material',
-      'Text 2',
-      'Text 3'
-  ];
-
-  public constructor(
-      private readonly matIconRegistry: MatIconRegistry,
-      private readonly domSanitizer: DomSanitizer) {
-      this.matIconRegistry.addSvgIcon(
-          'angular_logo',
-          this.domSanitizer.bypassSecurityTrustResourceUrl(
-              '../../../assets/angular.svg'
-          )
-      );
-  }
-}
+export class FooterComponent { }
