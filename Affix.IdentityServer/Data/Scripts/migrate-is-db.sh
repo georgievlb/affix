5891dc017b8f612e3a274ceec7027c8e26806fde
Lachezar Georgiev <georgiev.lb@gmail.com>
2023-03-06 14:58:55 +0200

    feat: add db migration container

--- Affix.IdentityServer/Data/Scripts/migrate-is-db.sh
+++ Affix.IdentityServer/Data/Scripts/migrate-is-db.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 set -e
+echo "Attempting to migrate database\n*******************************"
 
-echo "Determining os release:" >> /var/log/dbmigration.log
-cat /etc/os-release
-
-echo "Attempting to add a new entry to /etc/apt/sources.list" >> /var/log/dbmigration.log
-echo deb http://debian.org/debian stretch main >> /etc/apt/sources.list
-
-echo "Attempting to install curl"
-apt install curl
-
-echo "Attempting to download the dotnet-install script" >> /var/log/dbmigration.log
-curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
-
-chmod 744 dotnet-install.sh
-
-echo "Attempting to Install dotnet 6 sdk" >> /var/log/dbmigration.log
-./dotnet-install.sh -c 6.0
-
-echo "Attempting to install dotnet-ef" >> /var/log/dbmigration.log
+echo "Installing dotnet-ef"
 dotnet tool install --global dotnet-ef
 
-echo "Successfully migrated database" >> /var/log/dbmigration.log
+echo "Adding dotnet tools PATH variable for current session"
+export PATH="$PATH:/root/.dotnet/tools"
+
+echo "Migrating db"
+dotnet-ef database update -c AffixIdentityContext -p ./Affix.IdentityServer.csproj -v --connection "$1" 
+
+echo "Successfully migrated database\n*******************************"
