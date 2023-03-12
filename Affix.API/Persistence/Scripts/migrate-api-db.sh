661d08d84e2655aee8eba322c9a937c763a5d459
Lachezar Georgiev <georgiev.lb@gmail.com>
2023-03-12 09:01:15 +0200

    fix: add logging

--- Affix.API/Persistence/Scripts/migrate-api-db.sh
+++ Affix.API/Persistence/Scripts/migrate-api-db.sh
@@ -8,6 +8,12 @@ dotnet tool install --global dotnet-ef
 echo "Adding dotnet tools PATH variable for current session"
 export PATH="$PATH:/root/.dotnet/tools"
 
+echo "Connection string"
+echo $1
+
+echo "Environment variables"
+printenv
+
 echo "Migrating db"
 dotnet-ef database update -c AffixContext -p ./Affix.API.csproj -v --connection "$1" 
 
